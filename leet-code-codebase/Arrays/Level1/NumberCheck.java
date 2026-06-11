bd8ab47796b6f6e221653860f8a65fd8c665d6a2
rishu8925 <rishu.upadhyay_cs23@gla.ac.in>
2026-06-11 17:28:39 +0530

    [Rishu]add: added arrays level1 assignment

---
+++ leet-code-codebase/Arrays/Level1/NumberCheck.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+
+public class NumberCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int num : arr) {
+            if (num > 0) {
+                if (num % 2 == 0) {
+                    System.out.println(num + " is Positive Even");
+                } else {
+                    System.out.println(num + " is Positive Odd");
+                }
+            } else if (num < 0) {
+                System.out.println(num + " is Negative");
+            } else {
+                System.out.println(num + " is Zero");
+            }
+        }
+
+        if (arr[0] == arr[4]) {
+            System.out.println("First and last elements are equal");
+        } else if (arr[0] > arr[4]) {
+            System.out.println("First element is greater");
+        } else {
+            System.out.println("First element is less");
+        }
+
+        sc.close();
+    }
+}
