2da10dbdabb825623887e449fc44abf0b4c9fb0b
rishu8925 <rishu.upadhyay_cs23@gla.ac.in>
2026-06-15 14:41:16 +0530

    [Rishu]Add: add java methods assignment

---
+++ leet-code-codebase/JavaMethods/level1/ParkRounds.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class ParkRounds {
+    static double calculateRounds(double a, double b, double c) {
+        double perimeter = a + b + c;
+        return 5000 / perimeter;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter side 1: ");
+        double a = sc.nextDouble();
+
+        System.out.print("Enter side 2: ");
+        double b = sc.nextDouble();
+
+        System.out.print("Enter side 3: ");
+        double c = sc.nextDouble();
+
+        System.out.println("Rounds needed: " + calculateRounds(a, b, c));
+    }
+}
