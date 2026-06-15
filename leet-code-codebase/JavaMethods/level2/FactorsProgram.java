b363b24b75bfb4abae4d98c60767fdfaf88b5384
rishu8925 <rishu.upadhyay_cs23@gla.ac.in>
2026-06-15 14:07:46 +0530

    [Rishu]Add: add java methods assignment

---
+++ leet-code-codebase/JavaMethods/level2/FactorsProgram.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class FactorsProgram {
+
+    public static int[] findFactors(int num) {
+        int count = 0;
+
+        // Count factors
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                count++;
+            }
+        }
+
+        // Store factors
+        int[] factors = new int[count];
+        int index = 0;
+
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                factors[index++] = i;
+            }
+        }
+
+        return factors;
+    }
+
+    public static int sumFactors(int[] factors) {
+        int sum = 0;
+        for (int f : factors) {
+            sum += f;
+        }
+        return sum;
+    }
+
+    public static long productFactors(int[] factors) {
+        long product = 1;
+        for (int f : factors) {
+            product *= f;
+        }
+        return product;
+    }
+
+    public static double sumSquareFactors(int[] factors) {
+        double sum = 0;
+        for (int f : factors) {
+            sum += Math.pow(f, 2);
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int[] factors = findFactors(num);
+
+        System.out.print("Factors: ");
+        for (int f : factors) {
+            System.out.print(f + " ");
+        }
+
+        System.out.println("\nSum = " + sumFactors(factors));
+        System.out.println("Product = " + productFactors(factors));
+        System.out.println("Sum of Squares = " + sumSquareFactors(factors));
+    }
+}
